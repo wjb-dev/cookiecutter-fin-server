20a7a1913299f48ee381e604e1e615b69a75e18e
burkswill2 <burks.113@osu.edu>
2025-06-22 23:12:37 -0400

    New postgen setup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Tiny Cookiecutter hook that delegates everything to post_gen.runner."""
-from post_gen import main
+from .post_gen.runner import main
 
 if __name__ == "__main__":
     main()
