e57a39a93cd15d263675e80333d86fd32ec88bb6
burkswill2 <burks.113@osu.edu>
2025-06-22 23:53:25 -0400

    New postgen setup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, str(Path.cwd() / "hooks"))
 print("[DEBUG] hook sys.path[0] =", sys.path[0])
 
-from .post_gen import main
+from hooks.post_gen import main
 
 if __name__ == "__main__":
     main()
