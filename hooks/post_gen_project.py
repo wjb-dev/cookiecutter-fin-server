472cac4c8bd751696421e17065fdf3e64e935a67
burkswill2 <burks.113@osu.edu>
2025-06-22 23:21:31 -0400

    New postgen setup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import sys
 
+# Add the hooks directory to the sys.path
 HOOKS_DIR = Path(__file__).resolve().parent
 sys.path.insert(0, str(HOOKS_DIR))
 
-from .post_gen.runner import main
+from post_gen.runner import main
 
 if __name__ == "__main__":
     main()
